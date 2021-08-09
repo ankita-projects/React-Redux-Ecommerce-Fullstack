a5532ed6a09ee633ae80baea428b0f1be5464c9b
Ankita Bhatnagar <ankita.bhatnagar@outlook.com>
2021-08-09 14:22:35 +0200

    adding firebase authentication

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import {Switch, Route } from 'react-router-dom';
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import {createStore} from 'redux';
+
 
 import Register from "./pages/authorization/Register";
 import Login from "./pages/authorization/Login";
 import Home from "./pages/Home";
 import Header from "./Components/Nav/Header";
 import RegisterComplete from "./pages/authorization/RegisterComplete";
+
 
 const App = () => {
   return(
